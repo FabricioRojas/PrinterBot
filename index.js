2cebbcabce890a8c8a8004bbc1eac9d9467f6466
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2020-03-31 11:58:23 +0100

    Bot launch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api');
-const apiToken = require('./token.js');
-const bot = new TelegramBot(apiToken.get(), {polling: true});
+const config = require('./config.js');
 const ptp = require("pdf-to-printer");
-const whiteList = ["395625211", "339062381"];
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(config.getToken(), {polling: true});
+const whiteList = config.getWhiteList();
 
 console.log("** BOT LISTENING **");
 
@@ -10,19 +10,12 @@ bot.onText(/Start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Welcome " +  msg.from.first_name + ", I'll be your printer assistant.");  
 });
 
-bot.onText(/Time/, (msg) => {
-    bot.sendMessage(msg.chat.id, new Date());  
-});
-
 bot.on('document', (doc) => {
     console.log("document received: " , doc.from.id, whiteList.indexOf(doc.from.id+''), doc.document.file_name, doc.document.mime_type);
     if(whiteList.indexOf(doc.from.id+'') != -1){  
         let filePromise = bot.downloadFile(doc.document.file_id, "downloads");
-        const options = {
-            printer: "DCPL2500D"
-        };
         filePromise.then((filePath) => {
-            ptp.print(filePath, options)
+            ptp.print(filePath, config.getPrinter())
                 .then("then", console.log)
                 .catch("catch", console.error);
         });
@@ -36,12 +29,5 @@ bot.on('photo', (img) => {
 bot.on('message', (msg) => {  
     if(msg.text){
         console.log("Message received: " +  msg.text, msg.from.id, msg.from.first_name); 
-        // bot.sendMessage(msg.chat.id, , {
-        //     "reply_markup": {"keyboard": [["Start", "Time"],   ["Test"]]}
-        // });
-        // var what = "idiot";
-        // if (msg.text.includes(what)) {
-        //     bot.kickChatMember(msg.chat.id,  msg.from.id);
-        // }   
     }       
 });
